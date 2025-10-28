531884687debdd99a1cbff5b9799d3fc8ac34d50
Kisonam <kisonamplay@gmail.com>
2025-10-28 23:27:39 +0100

    feat: add Note, TaskItem, User entities and ThemeMode enum

---
+++ backend/src/Flowly.Domain/Entities/TaskItem.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace Flowly.Domain.Entities;
+
+public class TaskItem
+{
+
+}
---
+++ backend/src/Flowly.Domain/Enums/ThemeMode.cs
@@ -0,0 +1,7 @@
+namespace Flowly.Domain.Enums;
+
+public enum ThemeMode
+{
+    Normal = 0,      
+    LowStimulus = 1  
+}
